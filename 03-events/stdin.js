25c97213e072d4dbbded2422d55fc3e765747fba
William Koller <wkoller25@gmail.com>
2020-06-09 20:34:16 -0300

    refactor: remove readme

---
+++ 03-events/stdin.js
@@ -0,0 +1,15 @@
+const EventEmitter = require('events')
+class MeuEmissor extends EventEmitter {
+  
+}
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+meuEmissor.on(nomeEvento, function (click) {
+  console.log('um usuario clicou', click)
+})
+
+const stdin = process.openStdin()
+stdin.addListener('data', function (value) {
+  console.log(`Voce digitou: ${value.toString().trim()}`)
+})
--- README.md
+++
@@ -1,1 +0,0 @@
-# Training Node BR

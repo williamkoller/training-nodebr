aa8eb919fe041476d72e211d67a5dab2538497e2
William Koller <wkoller25@gmail.com>
2020-06-11 18:17:50 -0300

    fea: add listar and cadastrar with tests

---
+++ 06-cli/test.js
@@ -0,0 +1,30 @@
+const {
+    deepEqual,
+    ok
+} = require('assert')
+
+const database = require('./database')
+
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1
+}
+
+describe('Suite de manipulação de Herois', () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+    it('deve pesquisar um heroi usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const [resultado] = await database.listar(expected.id)
+        deepEqual(resultado, expected)
+    })
+
+    it('deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [atual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(atual, expected)
+    })
+})

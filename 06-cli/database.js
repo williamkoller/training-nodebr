9ab9704602b310602571496bbc219ed9cc3e961c
William Koller <wkoller25@gmail.com>
2020-06-23 23:59:05 -0300

    feat : add class Heroi

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,11 +1,6 @@
-const {
-    readFile,
-    writeFile
-} = require('fs')
+const { readFile, writeFile } = require('fs')
 
-const {
-    promisify
-} = require('util')
+const { promisify } = require('util')
 
 const readFileAsync = promisify(readFile)
 const writeFileAsync = promisify(writeFile)
@@ -13,7 +8,7 @@ class Database {
     constructor() {
         this.NOME_ARQUIVO = 'herois.json'
     }
-    async obterDadosArquivos() {
+    async obterDadosArquivo() {
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         return JSON.parse(arquivo.toString())
     }
@@ -22,23 +17,50 @@ class Database {
         return true
     }
     async cadastrar(heroi) {
-        const dados = await this.obterDadosArquivos()
+        const dados = await this.obterDadosArquivo()
         const id = heroi.id <= 2 ? heroi.id : Date.now()
         const heroiComId = {
             id,
-            ...heroi
+            ...heroi,
         }
-        const dadosFinal = [
-            ...dados,
-            heroiComId
-        ]
+        const dadosFinal = [...dados, heroiComId]
         const resultado = await this.escreverArquivo(dadosFinal)
         return resultado
     }
     async listar(id) {
-        const dados = await this.obterDadosArquivos()
-        const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
+        const dados = await this.obterDadosArquivo()
+        const dadosFiltrados = dados.filter((item) => (id ? item.id === id : true))
         return dadosFiltrados
+    }
+    async remover(id) {
+        if (!id) {
+            return await this.escreverArquivo([])
+        }
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex((item) => item.id === parseInt(id))
+        if (indice === -1) {
+            throw Error('O usuário informado não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
+    }
+    async atualizar(id, modificacoes) {
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if (indice === -1) {
+            throw Error('O heroi informado não existe')
+        }
+        const atual = dados[indice]
+        const objetoAtualizar = {
+            ...atual,
+            ...modificacoes
+        }
+        dados.splice(indice, 1)
+
+        return await this.escreverArquivo([
+            ...dados,
+            objetoAtualizar
+        ])
     }
 }
 

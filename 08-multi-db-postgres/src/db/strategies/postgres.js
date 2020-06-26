9d58b0695db7d4addde88ced720dc758e24a2e18
William Koller <wkoller25@gmail.com>
2020-06-26 01:49:40 -0300

    feat: add strategy postgres

--- 08-multi-db-postgres/src/db/strategies/postgres.js
+++ 08-multi-db-postgres/src/db/strategies/postgres.js
@@ -1,14 +1,65 @@
 const ICrud = require('./interfaces/interfaceCrud')
 
+const { Sequelize } = require('sequelize')
+
 class Postgres extends ICrud {
-    constructor() {
-        super()
-    }
-
-    create(item) {
-        console.log('Item foi salvo em Postgres')
-    }
-
+	constructor() {
+		super()
+		this._driver = null
+		this._herois = null
+		this._connect()
+	}
+	async isConnected() {
+		try {
+			await this._driver.authenticate()
+			return true
+		} catch (error) {
+			console.log('Fail!', error)
+			return false
+		}
+	}
+	async defineModel() {
+		this._herois = driver.define(
+			'herois',
+			{
+				id: {
+					type: Sequelize.INTEGER,
+					required: true,
+					primaryKey: true,
+					autoIncrement: true,
+				},
+				nome: {
+					type: Sequelize.STRING,
+					required: true,
+				},
+				poder: {
+					type: Sequelize.STRING,
+					required: true,
+				},
+			},
+			{
+				tableName: 'herois',
+				freezeTableName: false,
+				timestamps: false,
+			}
+		)
+		await Herois.sync()
+	}
+	create(item) {
+		console.log('Item foi salvo em Postgres')
+	}
+	_connect() {
+		this._driver = new Sequelize(
+			'heroes',
+			'williamkoller',
+			'minhasenhasecreta',
+			{
+				host: 'localhost',
+				dialect: 'postgres',
+				quoteIdentifiers: false,
+			}
+		)
+	}
 }
 
 module.exports = Postgres

000b959360d8fbc1aa2f1d82db36132909ad2c23
William Koller <wkoller25@gmail.com>
2020-06-28 01:31:36 -0300

    feat: add connect

--- 08-multi-db-postgres/src/db/strategies/postgres.js
+++ 08-multi-db-postgres/src/db/strategies/postgres.js
@@ -7,7 +7,6 @@ class Postgres extends ICrud {
 		super()
 		this._driver = null
 		this._herois = null
-		this._connect()
 	}
 	async isConnected() {
 		try {
@@ -19,7 +18,7 @@ class Postgres extends ICrud {
 		}
 	}
 	async defineModel() {
-		this._herois = driver.define(
+		this._herois = this._driver.define(
 			'herois',
 			{
 				id: {
@@ -43,12 +42,13 @@ class Postgres extends ICrud {
 				timestamps: false,
 			}
 		)
-		await Herois.sync()
+		await this._herois.sync()
 	}
-	create(item) {
-		console.log('Item foi salvo em Postgres')
+	async create(item) {
+		const { dataValues } = await this._herois.create(item)
+		return dataValues
 	}
-	_connect() {
+	async connect() {
 		this._driver = new Sequelize(
 			'heroes',
 			'williamkoller',
@@ -59,6 +59,7 @@ class Postgres extends ICrud {
 				quoteIdentifiers: false,
 			}
 		)
+		await this.defineModel()
 	}
 }
 
--- 08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ 08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -3,11 +3,20 @@ const Postgres = require('../db/strategies/postgres')
 const Context = require('../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres())
+const MOCK_HEROI_CADASTRAR = { nome: 'Gaviao Negro', poder: 'Flexas' }
 
-describe('Postgres Strategy', function () {
+describe('Postgres Strategy ', function () {
 	this.timeout(Infinity)
+	this.beforeAll(async function () {
+		await context.connect()
+	})
 	it('Postgres SQL Connection', async function () {
 		const result = await context.isConnected()
 		assert.equal(result, true)
+	})
+	it.only('Cadastrar', async function () {
+		const result = await context.create(MOCK_HEROI_CADASTRAR)
+		delete result.id
+		assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
 	})
 })
